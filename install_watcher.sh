309c5bbca4f8153b1de52e6d18f5885b42bf0b39
Zain <115614618+zainantum@users.noreply.github.com>
2023-12-13 16:19:10 +0700

    Update install_watcher.sh

--- install_watcher.sh
+++ install_watcher.sh
@@ -1,6 +1,6 @@
-rm -rf watch.py* && wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/watch.py?token=GHSAT0AAAAAACHQUDN2ETEG3LH67UT5E2LSZLZOTOQ && chmod +x watch.py
-wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/rep1.json?token=GHSAT0AAAAAACHQUDN2CMOJMHX3QKYMO53SZLZOVOA
-wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/rep2.json?token=GHSAT0AAAAAACHQUDN3PMNZI3PL27S4A4USZLZOVWA
+rm -rf watch.py* && wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/watch.py
+wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/rep1.json
+wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/rep2.json
 echo -e "\e[1m\e[32m1. Installing watcher... \e[0m" && sleep 2
 pathFileRestart=$(realpath watch.py)
 if ! crontab -l | grep -q 'watch.py';
