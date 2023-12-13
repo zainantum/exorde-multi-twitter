7bd8e1f1d6211dc44f0c62e210bcb27388881a66
Zain <115614618+zainantum@users.noreply.github.com>
2023-12-13 16:22:12 +0700

    Update install_watcher.sh

--- install_watcher.sh
+++ install_watcher.sh
@@ -1,4 +1,4 @@
-rm -rf watch.py* && wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/watch.py
+rm -rf watch.py* && wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/watch.py && chmod +x watch.py
 wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/rep1.json
 wget https://raw.githubusercontent.com/zainantum/exorde-multi-twitter/main/rep2.json
 echo -e "\e[1m\e[32m1. Installing watcher... \e[0m" && sleep 2
@@ -7,8 +7,10 @@ if ! crontab -l | grep -q 'watch.py';
 then
     echo "Adding auto watch script to cronjob"
     crontab -l > mycron
-    echo "*/30 * * * * $pathFileRestart" >> mycron
+    echo "*/30 * * * * python3 $pathFileRestart" >> mycron
     crontab mycron
     rm mycron
 fi
+echo "Install library"
+pip install docker
 echo '=============== DONE ==================='
